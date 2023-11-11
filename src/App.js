f13f5b84b1a6ef409823a327cd683de884eb060e
randipsbh <randipsbh@gmail.com>
2023-11-11 13:58:05 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Counter from "./Counter";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
+
+      <Counter>
+        <Counter.Decrease icon="-" />
+        <Counter.Count />
+        <Counter.Increase icon="+" />
+        <Counter.Label>My super flexible counter</Counter.Label>
+      </Counter>
+      <div>
+        <Counter>
+          <Counter.Decrease icon="-" />
+          <Counter.Count />
+          <Counter.Increase icon="+" />
+        </Counter>
+      </div>
+    </div>
+  );
+}
